1f27650ae0fa7c90cab1c2bb227405f87c20c337
Amanda Ogletree <amandaogletree19@gmail.com>
2026-02-01 16:51:05 -0500

    Updated userController.js with new error handling utility functions.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,38 @@
 //Functional Logic for User Routes
 const User = require('../models/User');
 const { signToken } = require('../utils/auth')
+const { badRequestErrorObj, createErrorObject } = require('../utils/errorhandling')
 
 const registerUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     const token = signToken(user);
     res.status(201).json({ token, user });
-  } catch (err) {
-    console.error('[ An Error Occured Creating new User ] ', err)
-    res.status(400).json({ error: 'Error creating new user. User already exist.' });
+  } catch (error) {
+    res.status(400).json(badRequestErrorObj("creating new user. User may already exist", error));
   }
 }
 
 const loginUser = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
-  if (!user) {
-    return res.status(400).json({ message: "Password or username incorrect." });
+    if (!user) {
+      return res.status(400).json(createErrorObject("Password or username incorrect or left empty.", "BAD_REQUEST"));
+    }
+    const correctPw = await user.isCorrectPassword(req.body.password);
+
+    if (!correctPw) {
+      return res.status(400).json(createErrorObject("Password or username incorrect or left empty.", "BAD_REQUEST"));
+    }
+
+    const token = signToken(user);
+    res.json({ token, user });
+
+  } catch (error) {
+    res.status(400).json(badRequestErrorObj("logging in user", error));
   }
 
-  const correctPw = await user.isCorrectPassword(req.body.password);
-
-  if (!correctPw) {
-    return res.status(400).json({ message: 'Password or username incorrect.' });
-  }
-
-  const token = signToken(user);
-  res.json({ token, user });
 }
 
 module.exports = {

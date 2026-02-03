e83b59805a9d44cced4d504f6e652fb750540e35
Amanda Ogletree <amandaogletree19@gmail.com>
2026-02-03 15:16:08 -0500

    Updated response for registerUser and loginUser to return data in array (brackets []) for better consistant data formatting.

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const registerUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     const token = signToken(user);
-    res.status(201).json({ token, user });
+    res.status(201).json([{ token, user }]);
   } catch (error) {
     res.status(400).json(badRequestErrorObj("creating new user. User may already exist", error));
   }
@@ -27,7 +27,7 @@ const loginUser = async (req, res) => {
     }
 
     const token = signToken(user);
-    res.json({ token, user });
+    res.json([{ token, user }]);
 
   } catch (error) {
     res.status(400).json(badRequestErrorObj("logging in user", error));
